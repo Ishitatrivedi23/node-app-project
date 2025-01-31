951b8235dafc0354662ed638bd5cc34f34c7c9b4
AditiRaghav7 <adiraghav16r@gmail.com>
2025-01-31 16:29:54 +0530

    added

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,18 +1,19 @@
 const mysql = require("mysql2");
 
-
 // Create a connection to the database
 const connection = mysql.createConnection({
   host: 'localhost',  // Replace with your MySQL host
   user: 'root',  // Replace with your MySQL username
   password: 'Aditi@1122',  // Replace with your MySQL password
   database: 'new',  // Replace with your MySQL database name
- 
 });
 
 // open the MySQL connection
-connection.connect(error => {
-  if (error) throw error;
+connection.connect((error) => {
+  if (error) {
+    console.error("Error connecting to the database: ", error.message);
+    throw error;
+  }
   console.log("Successfully connected to the MYSQL database.");
 });
 
