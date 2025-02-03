c238b18ca12a9f4b5eebe2e644b54f24099c83bd
AditiRaghav7 <adiraghav16r@gmail.com>
2025-02-03 15:30:39 +0530

    done

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,40 +1,19 @@
 const mysql = require("mysql2");
-var express = require("express");
-var app = express();
+
 
 // Create a connection to the database
-const con = mysql.createConnection({
-  host: "localhost",    // or use your MySQL server IP
+const connection = mysql.createConnection({
+  host: "127.0.0.1",
   user: "root",
   password: "Aditi@1122",
-  database: "employees_db",  // Specify your database name
-  connectTimeout: 50000
+  port: "3306",
+  database: "employees_db"
 });
 
-// Establish the connection
-con.connect((err) => {
-  if (err) {
-    console.log("Connection error:", err);
-  } else {
-    console.log("Connected to MySQL database.");
-  }
+// open the MySQL connection
+connection.connect(error => {
+  if (error) throw error;
+  console.log("Successfully connected to the MYSQL database.");
 });
 
-// Define a route to fetch data
-app.get("/fetch", (req, res) => {
-  con.query("SELECT * FROM employees_db", function (err, result, fields) {
-    if (err) {
-      console.log("Error fetching data:", err);
-      res.status(500).send("Error fetching data.");
-    } else {
-      var r = JSON.parse(JSON.stringify(result));
-      res.status(200).json(r); // Send the result as a JSON response
-    }
-  });
-});
-
-// Start the Express server
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+module.exports = connection;
