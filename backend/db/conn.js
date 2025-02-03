d39f64e357e4c491d219be6a6db431b21d5185b9
AditiRaghav7 <adiraghav16r@gmail.com>
2025-02-03 15:35:41 +0530

    done

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   host: "127.0.0.1",      // Ensure MySQL is running on this IP or adjust accordingly
   user: "root",
   password: "Aditi@1122",
-  port: 8000,             // Use a number for the port, not a string
+  port: 3306,             // Use a number for the port, not a string
   database: "employees_db"
 });
 
