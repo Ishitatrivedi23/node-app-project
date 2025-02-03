df1e09cac48e54125e885caff0002899eaba48a7
AditiRaghav7 <adiraghav16r@gmail.com>
2025-02-03 12:25:58 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const appRoutes = require('./routes/appRoutes')
+const appRoutes = require('./routes')
 const bodyParser = require('body-parser');
 const cors = require('cors')
 // const cors = require("cors");
