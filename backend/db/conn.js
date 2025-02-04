eac217fae921ffc6efba8611e1aca0a6a0a07ce1
AditiRaghav7 <adiraghav16r@gmail.com>
2025-02-04 11:18:55 +0530

    done

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-  host: "172.31.39.171",      // Ensure MySQL is running on this IP or adjust accordingly
+  host: "ip-172-31-39-171", // Ensure MySQL is running on this IP or adjust accordingly
   user: "root",
   password: "Aditi@1122",
   port: 3306,             // Use a number for the port, not a string
