6ef8b0dfaf6911b86ea2379e19ac19e731922de3
AditiRaghav7 <adiraghav16r@gmail.com>
2025-02-04 11:49:47 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const connection = require('./db/conn');  // Make sure to import the connection 
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
