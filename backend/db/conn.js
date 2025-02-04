4b1c6718e1d4fcf12792162e0c88acaae2f26112
AditiRaghav7 <adiraghav16r@gmail.com>
2025-02-04 12:41:05 +0530

    done

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-host: "172.31.39.171",
+host: "13.61.100.220",
   user: "root",
   password: "Aditi@1122",
   port: 3306,   
