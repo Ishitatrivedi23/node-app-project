f1d4a886c182f686b9ecc0e7bb8dfcdc08b88ad0
AditiRaghav7 <adiraghav16r@gmail.com>
2025-02-05 21:47:41 +0530

    done

--- frontEnd/routes.js
+++ frontEnd/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const fetch = require('node-fetch')
 const router = express.Router()
 
-const host= 'http://localhost:8000'
+const host= 'http://13.60.104.168:8000'
 
 router.get('/', (req, res)=>{
     res.render('index')
