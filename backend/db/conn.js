5df9180ad593e1802bbdff72503acec00c7e6230
Ubuntu <ubuntu@ip-172-31-38-8.eu-north-1.compute.internal>
2025-02-12 04:34:55 +0000

    changes made

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-host: "my-mysql-container",
+  host: "mysql",
   user: "root",
   password: "Aditi@1122",
   port: 3306,   
